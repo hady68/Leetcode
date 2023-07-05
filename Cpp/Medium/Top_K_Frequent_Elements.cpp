21ce52bcdd09b90aee620911ee1e4b4f9e74c68f
Hardikk Kamboj <64458111+hady68@users.noreply.github.com>
2023-07-05 16:42:42 +0530

    Update Top_K_Frequent_Elements.cpp

--- Cpp/Medium/Top_K_Frequent_Elements.cpp
+++ Cpp/Medium/Top_K_Frequent_Elements.cpp
@@ -35,28 +35,47 @@ public:
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+          
         int n = nums.size();
-        
         unordered_map<int, int> m;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < nums.size(); i++) {
             m[nums[i]]++;
         }
-        
-        vector<vector<int>> buckets(n + 1);
+
+        // ex -> 1,1,1,2,2,3 -> 1->3, 2->2, 3->1
+       vector<vector<int>> buckets(n + 1);
+
         for (auto it = m.begin(); it != m.end(); it++) {
+
             buckets[it->second].push_back(it->first);
         }
+
+        // buckets -> 1->3 , 2->2 , 3->1
         
         vector<int> result;
-        
+
+        // k = 2 -> ans ={1,2}
+        //. loop from end as max bucket stores element with most frequency -> 1 
+
         for (int i = n; i >= 0; i--) {
+            // will run only till k elements condition is fulfilled
             if (result.size() >= k) {
                 break;
             }
+
+            // it's not empty, it means there are elements with a frequency of i.
+            // The elements from buckets[i] are inserted at the end of the result vector using the insert function.
+            
             if (!buckets[i].empty()) {
                 result.insert(result.end(), buckets[i].begin(), buckets[i].end());
             }
         }
+        
+        return result;
+
+    }
+};
+
         
         return result;
     }
