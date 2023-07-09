7aa52fb1de3aa399f017af8f0c3162ece60c34f8
Hardikk Kamboj <64458111+hady68@users.noreply.github.com>
2023-07-09 11:40:13 +0530

    Create Encode_Decode_Strings.cpp

---
+++ Cpp/Medium/Encode_Decode_Strings.cpp
@@ -0,0 +1,69 @@
+// Description
+// Design an algorithm to encode a list of strings to a string. The encoded string is then sent over the network and is decoded back to the original list of strings.
+
+// Please implement encode and decode
+
+// Example
+// Example1
+
+// Input: ["lint","code","love","you"]
+// Output: ["lint","code","love","you"]
+// Explanation:
+// One possible encode method is: "lint:;code:;love:;you"
+// Example2
+
+// Input: ["we", "say", ":", "yes"]
+// Output: ["we", "say", ":", "yes"]
+// Explanation:
+// One possible encode method is: "we:;say:;:::;y
+
+// Solution
+
+/*
+    Design algorithm to encode/decode: list of strings <-> string
+
+    Encode/decode w/ non-ASCII delimiter: {len of str, "#", str}
+
+    Time: O(n)
+    Space: O(1)
+*/
+
+class Codec {
+public:
+
+    // Encodes a list of strings to a single string.
+    string encode(vector<string>& strs) {
+        string result = "";
+        
+        for (int i = 0; i < strs.size(); i++) {
+            string str = strs[i];
+            result += to_string(str.size()) + "#" + str;
+        }
+        
+        return result;
+    }
+
+    // Decodes a single string to a list of strings.
+    vector<string> decode(string s) {
+        vector<string> result;
+        
+        int i = 0;
+        while (i < s.size()) {
+            int j = i;
+            while (s[j] != '#') {
+                j++;
+            }
+            int length = stoi(s.substr(i, j - i));
+            string str = s.substr(j + 1, length);
+            result.push_back(str);
+            i = j + 1 + length;
+        }
+        
+        return result;
+    }
+private:
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec;
+// codec.decode(codec.encode(strs));
