3b719ab756f5de6e623719b918b4d4ac6a62aeae
Hardikk Kamboj <64458111+hady68@users.noreply.github.com>
2023-07-09 09:49:45 +0530

    Create ValidSudoku.cpp

---
+++ Cpp/Medium/ValidSudoku.cpp
@@ -0,0 +1,51 @@
+// Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+// Each row must contain the digits 1-9 without repetition.
+// Each column must contain the digits 1-9 without repetition.
+// Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+// Note:
+
+// A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+// Only the filled cells need to be validated according to the mentioned rules.
+
+
+//Solution
+
+/*
+    Determine if a 9x9 Sudoku board is valid (no repeats)
+
+    Boolean matrices to store seen values. Check rows, cols, 3x3 sub-boxes
+
+    Time: O(cnt^2)
+    Space: O(cnt^2)
+*/
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        const int cnt = 9;
+        bool row[cnt][cnt] = {false};
+        bool col[cnt][cnt] = {false};
+        bool sub[cnt][cnt] = {false};
+        
+        for(int r = 0; r < cnt; ++r){
+            for(int c = 0; c < cnt; ++c){
+                if(board[r][c] == '.')
+                    continue; // if not number pass
+                
+                int idx = board[r][c] - '0' - 1; //char to num idx
+                int area = (r/3) * 3 + (c/3);
+                
+                //if number already exists
+                if(row[r][idx] || col[c][idx] || sub[area][idx]){
+                    return false;
+                }
+                
+                row[r][idx] = true;
+                col[c][idx] = true;
+                sub[area][idx] = true;
+            }
+        }
+        return true;
+    }
+};
