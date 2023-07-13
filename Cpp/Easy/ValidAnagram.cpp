da6f4e3c128f10099013e2a80f0c5574a747f114
Hardikk Kamboj <64458111+hady68@users.noreply.github.com>
2023-07-14 00:08:11 +0530

    Update ValidAnagram.cpp

--- Cpp/Easy/ValidAnagram.cpp
+++ Cpp/Easy/ValidAnagram.cpp
@@ -17,20 +17,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()) return false;
-        
-        unordered_map<char,int> smap;
-        unordered_map<char,int> tmap;
-        
-        for(int i = 0; i < s.size(); i++){
-            smap[s[i]]++;
-            tmap[t[i]]++;
-        }
-        
-        for(int i = 0; i < smap.size(); i++){
-            if(smap[i] != tmap[i]) return false;
-        }
-        return true;
+      if(s.size() != t.size()){
+       return false;
+      }
+
+     unordered_map<char,int>smapped;
+     unordered_map<char,int>tmapped;
+
+     for(int i = 0; i<s.size(); i++){
+      smapped[s[i]]++;
+      tmapped[t[i]]++;
+     }
+
+     for(int i=0; i<s.size();i++){
+
+   if(smapped[i]! tmapped[i]) return false;     
+     }
+     return true;
     }
 };
 
