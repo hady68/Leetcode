7a38182483ac7994b4fdfad6c4b8a969ed7d4df1
Hardikk Kamboj <64458111+hady68@users.noreply.github.com>
2023-07-14 10:13:29 +0530

    Create SetMatrixZeroes.cpp

---
+++ Cpp/Medium/SetMatrixZeroes.cpp
@@ -0,0 +1,98 @@
+// // Description
+// Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+// You must do it in place.
+
+// Example 1:
+// Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+// Output: [[1,0,1],[0,0,0],[1,0,1]]
+
+// Example 2:
+// Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+// Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+ 
+// Constraints:
+// m == matrix.length
+// n == matrix[0].length
+// 1 <= m, n <= 200
+// -231 <= matrix[i][j] <= 231 - 1
+ 
+// Follow up:
+// A straightforward solution using O(mn) space is probably a bad idea.
+// A simple improvement uses O(m + n) space, but still not the best solution.
+// Could you devise a constant space solution?
+
+
+// Brute -> Solved using Matrix with Extra space. TC : O((NM)(N+M)), SC : O(N*M)
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>> visited = matrix;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == 0){
+                    for(int k=0; k<m; k++){
+                        visited[i][k] = 0;
+                    }
+                    for(int k=0; k<n; k++){
+                        visited[k][j] = 0;
+                    }
+                }
+            }
+        }
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                matrix[i][j] = visited[i][j];
+            }
+        }
+    }
+};
+
+
+
+// withou extra space -> mark all non zero in relevant ror with a num and then replace those num with zero at the end
+//Time Complexity: O((N*M)*(N + M)) + O(N*M)
+// Space -> O(1)
+class Solution {
+public:
+    void markrow(vector<vector<int>>& matrix, int i, int n) {
+        for (int j = 0; j < n; j++) {
+            if (matrix[i][j] != 0) {
+                matrix[i][j] = -999;
+            }
+        }
+    }
+
+    void markcol(vector<vector<int>>& matrix, int j, int m) {
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][j] != 0) {
+                matrix[i][j] = -999;
+            }
+        }
+    }
+
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    markrow(matrix, i, n);
+                    markcol(matrix, j, m);
+                }
+            }
+        }
+
+        // Mark all -1 as 0
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == -999) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
