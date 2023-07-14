7b6188b634397aa1e6db20f287b4ece385bb7fd3
Hardikk Kamboj <64458111+hady68@users.noreply.github.com>
2023-07-14 19:30:34 +0530

    Update SetMatrixZeroes.cpp

--- Cpp/Medium/SetMatrixZeroes.cpp
+++ Cpp/Medium/SetMatrixZeroes.cpp
@@ -132,4 +132,31 @@ vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m) {
     }
 
 
-// OPTIMAL 
+// OPTIMAL -> You can use the first row as the column of the matrix and first column as the row of the matrix.
+//Then take a variable to use it as a flag to where to mark a zero and where not to.
+//Then traverse the matrix from the back and start marking zeros.
+
+
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int col0 = 1;
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        for(int i = 0; i < rows; i++){
+            if(matrix[i][0] == 0) 
+            col0 = 0;
+            for(int j = 1; j < cols; j++)
+                if(matrix[i][j] == 0)
+                    matrix[i][0] = matrix[0][j] = 0;
+        }
+        for(int i = rows-1; i >= 0; i--){
+            for(int j = cols-1; j >= 1; j--)
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            if(col0 == 0)
+            matrix[i][0] = 0;
+        }
+    }
+};
