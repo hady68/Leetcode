965c9d01486881cedc4742587a33b7abddd66c80
Hardikk Kamboj <64458111+hady68@users.noreply.github.com>
2023-07-19 23:31:41 +0530

    Create RotateArray.cpp

---
+++ Cpp/Medium/RotateArray.cpp
@@ -0,0 +1,78 @@
+// Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,4,5,6,7], k = 3
+// Output: [5,6,7,1,2,3,4]
+// Explanation:
+// rotate 1 steps to the right: [7,1,2,3,4,5,6]
+// rotate 2 steps to the right: [6,7,1,2,3,4,5]
+// rotate 3 steps to the right: [5,6,7,1,2,3,4]
+// Example 2:
+
+// Input: nums = [-1,-100,3,99], k = 2
+// Output: [3,99,-1,-100]
+// Explanation: 
+// rotate 1 steps to the right: [99,-1,-100,3]
+// rotate 2 steps to the right: [3,99,-1,-100]
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -231 <= nums[i] <= 231 - 1
+// 0 <= k <= 105
+
+
+
+// Using reverse functiion
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k = k%nums.size();
+        // rotate all ex -> 1 2 3 4 5 6 7 k =3 
+        reverse(nums.begin(), nums.end()); // 7 6 5 4 3 2 1 
+       // reverse the last k elements 
+        reverse(nums.begin(), nums.begin()+k); // 7 6 5 1 2 3 4  
+        // reverse first k elements 
+        reverse(nums.begin()+k, nums.end()); // 5 6 7 1 2 3 4 
+
+    }
+};
+
+
+// Rotate all the elements by 1 position k times.
+class Solution {
+public:
+	void rotateByOne(vector<int> &nums){
+		int n = nums.size(), temp = nums[n-1];
+		for(int i=n-1; i>0; i--){
+			nums[i] = nums[i-1];
+		}
+			nums[0] = temp;
+	}
+    void rotate(vector<int>& nums, int k) {
+        for(int i=0; i<k; i++) rotateByOne(nums);
+    }
+};
+
+
+// 
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        
+        
+        vector<int> temp(nums.size());
+        for(int i=0; i<nums.size();i++){
+
+        temp[(i+k)%nums.size()] = nums[i];
+
+        }
+
+        nums = temp;
+    }
+};
