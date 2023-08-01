daab84ffb37d558d1c3d2b141dddea30af888807
Hardikk Kamboj <64458111+hady68@users.noreply.github.com>
2023-08-02 02:50:59 +0530

    Update KokoEatingBananas.cpp

--- Cpp/Medium/KokoEatingBananas.cpp
+++ Cpp/Medium/KokoEatingBananas.cpp
@@ -16,3 +16,45 @@
 // Input: piles = [30,11,23,4,20], h = 6
 // Output: 23
 
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        
+        int low = 1;
+        int high = 0;
+        
+
+        for(int i=0; i<piles.size(); i++){
+
+            if(piles[i]>high){
+                high = piles[i];
+            }
+        }
+
+    while(low<=high){
+        int mid = low+(high-low)/2;
+        long long int reqtime = 0;
+        for(int i=0; i<=piles.size()-1; i++){
+
+            if(piles[i]%mid!=0){
+                reqtime += piles[i]/mid + 1; 
+            }
+            else{
+                reqtime += piles[i]/mid;
+
+                }
+    }
+        if(reqtime>h){
+            low = mid+1;
+        }
+        else{
+            high = mid-1;
+        }
+        
+    }    
+
+    return low;
+
+    }
+
+};
