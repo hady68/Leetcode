8a45d3b6d85cba44005ab93203627b4e2c627ed4
Hardikk Kamboj <64458111+hady68@users.noreply.github.com>
2023-08-06 23:23:29 +0530

    Update FindMinimumRotatedSortedArray.cpp

--- Cpp/Medium/FindMinimumRotatedSortedArray.cpp
+++ Cpp/Medium/FindMinimumRotatedSortedArray.cpp
@@ -13,9 +13,3 @@
 // Input: nums = [4,5,6,7,0,1,2]
 // Output: 0
 // Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
-
-// Example 3:
-// Input: nums = [11,13,15,17]
-// Output: 11
-// Explanation: The original array was [11,13,15,17] and it was rotated 4 times. 
-
