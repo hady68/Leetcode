01120ab02dcb92719c7d6bd5faf784be759c384c
Hardikk Kamboj <64458111+hady68@users.noreply.github.com>
2023-09-17 11:24:22 +0530

    Update JewelsandStone.cpp

--- Cpp/Easy/JewelsandStone.cpp
+++ Cpp/Easy/JewelsandStone.cpp
@@ -19,3 +19,41 @@
 // 1 <= jewels.length, stones.length <= 50
 // jewels and stones consist of only English letters.
 // All the characters of jewels are unique.
+
+// brute force -> using 2 loops checking the stones arr if jewels is there 
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        
+       // for each stone check if a jewel
+        int count = 0;
+       for(int i=0; i<stones.size(); i++){
+
+           for(int j=0; j<jewels.size(); j++){
+               if (stones[i] == jewels[j])
+               count++;
+           }
+       }
+       return count;
+    }
+};
+
+// better -> using a hashmap 
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        
+      int ans=0; 
+    map<char,int>count;
+
+    //counting all stones
+    for(int i=0;i<stones.size();i++)
+        count[stones[i]]++;          
+
+    //sum all valid jewels stone
+    for(int i=0;i<jewels.size();i++)
+        ans+=count[jewels[i]];           
+
+    return ans;
+    }
+};    
