7da629d222cc6af01d759acfa2229b27fe4e789f
Hardikk Kamboj <64458111+hady68@users.noreply.github.com>
2023-09-29 21:58:59 +0530

    Update 27RemoveElement.cpp

--- Leetcode/Top-150/27RemoveElement.cpp
+++ Leetcode/Top-150/27RemoveElement.cpp
@@ -44,3 +44,16 @@
 // 0 <= nums.length <= 100
 // 0 <= nums[i] <= 50
 // 0 <= val <= 100
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[count++] = nums[i];
+            }
+        }
+        return count;
+    }
+};
