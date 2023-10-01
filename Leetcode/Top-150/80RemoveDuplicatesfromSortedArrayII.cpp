cd78854ce04ea4c9e6a6430e561defccd9f9aeff
Hardikk Kamboj <64458111+hady68@users.noreply.github.com>
2023-10-01 15:01:30 +0530

    Create 80RemoveDuplicatesfromSortedArrayII.cpp

---
+++ Leetcode/Top-150/80RemoveDuplicatesfromSortedArrayII.cpp
@@ -0,0 +1,100 @@
+// Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice. The relative order of the elements should be kept the same.
+
+// Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+// Return k after placing the final result in the first k slots of nums.
+
+// Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+// Custom Judge:
+
+// The judge will test your solution with the following code:
+
+// int[] nums = [...]; // Input array
+// int[] expectedNums = [...]; // The expected answer with correct length
+
+// int k = removeDuplicates(nums); // Calls your implementation
+
+// assert k == expectedNums.length;
+// for (int i = 0; i < k; i++) {
+//     assert nums[i] == expectedNums[i];
+// }
+// If all assertions pass, then your solution will be accepted.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,1,1,2,2,3]
+// Output: 5, nums = [1,1,2,2,3,_]
+// Explanation: Your function should return k = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+// Example 2:
+
+// Input: nums = [0,0,1,1,1,1,2,3,3]
+// Output: 7, nums = [0,0,1,1,2,3,3,_,_]
+// Explanation: Your function should return k = 7, with the first seven elements of nums being 0, 0, 1, 1, 2, 3 and 3 respectively.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 3 * 104
+// -104 <= nums[i] <= 104
+// nums is sorted in non-decreasing order.
+
+
+// Do what is gievn via 2 pointer TC -> O(n)
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        // Define at most k times of duplicate numbers
+        const int k = 2;
+
+        // Check if it is an empty array
+        if (nums.empty()) return 0;
+
+        // Start pointer of new array
+        int m = 1;
+
+        // Count the times of duplicate numbers occurrence
+        int count = 1;
+
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] == nums[i - 1]) {
+                if (count < k) {
+                    nums[m++] = nums[i];
+                }
+                count++;
+            } else {
+                count = 1;
+                nums[m++] = nums[i];
+            }
+        }
+        return m;
+    }
+};
+
+
+
+
+// TC -> O(N)
+class Solution {
+public:
+   int removeDuplicates(vector<int>& nums) {
+    int i = 0;
+    for (int n : nums){
+
+        if (i < 2 || n > nums[i-2])
+            nums[i++] = n;
+    }
+    return i;
+}
+};
+
+//Basically what the solution is doing ,
+// for every element we are checking the element that is two indexes before it...if this element is bigger tahn it we are assigning it a position else we don't.
+// suppose the array is [1,1,1,2,2,3,3]...and i=0
+// so the condition passes two times because i<2 for i=0 and i=1 then array becomes-->[1,1....] and i=2
+// Now, element is 1 and the element two index before is also 1 so we will don't consider this 1, and we will also not update i.
+// Now the element is 2 and the element two index before it is 1 , it is bigger than that so we will assign it a position and also increment i by one. array becomes--> [1,1,2..] and i=3...and so on.
