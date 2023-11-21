104684deeec8709b304ad3c6757e947de6e5f521
Hardikk Kamboj <64458111+khardikk@users.noreply.github.com>
2023-11-21 20:26:26 +0530

    Update frequency-of-the-most-frequent-element.cpp

--- Striver A-Z Sheet/Step-1/Hashing/frequency-of-the-most-frequent-element.cpp
+++ Striver A-Z Sheet/Step-1/Hashing/frequency-of-the-most-frequent-element.cpp
@@ -33,3 +33,30 @@
 // 1 <= k <= 105
 
 
+// Solution 1 -> Sort the arr and use sliding window Time: O(nlog⁡n) due to sorting + O(n) for sliding window, making it O(nlog⁡n)
+
+Space: O(log⁡n) for sorting
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int left = 0, ans = 0;
+        long curr = 0;
+        
+        for (int right = 0; right < nums.size(); right++) {
+            long target = nums[right];
+            curr += target;
+            
+            while ((right - left + 1) * target - curr > k) {
+                curr -= nums[left];
+                left++;
+            }
+            
+            ans = max(ans, right - left + 1);
+        }
+        
+        return ans;
+    }
+};
+
+
