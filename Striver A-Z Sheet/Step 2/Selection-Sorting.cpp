bf5d2ec40fec631cefdf69aaf9cea231738c28fe
Hardikk Kamboj <64458111+khardikk@users.noreply.github.com>
2023-11-21 20:45:54 +0530

    Create Selection-Sorting.cpp

---
+++ Striver A-Z Sheet/Step 2/Selection-Sorting.cpp
@@ -0,0 +1,34 @@
+// Problem Statement: Given an array of N integers, write a program to implement the Selection sorting algorithm.
+
+// Examples:
+
+// Example 1:
+// Input: N = 6, array[] = {13,46,24,52,20,9}
+// Output: 9,13,20,24,46,52
+// Explanation: After sorting the array is: 9, 13, 20, 24, 46, 52
+
+// Example 2:
+// Input: N=5, array[] = {5,4,3,2,1}
+// Output: 1,2,3,4,5
+// Explanation: After sorting the array is: 1, 2, 3, 4, 5
+
+// Approach:
+
+// The algorithm steps are as following
+// 1. run outer loop to run on each element
+// 2. inner loop to select the minimum element and swap it with the first in that range
+//  TC -> O(N)^2 SC -> O(1)
+
+void selection_sort(int arr[], int n) {
+  // selection sort
+  for (int i = 0; i < n - 1; i++) {
+    int mini = i;
+    for (int j = i + 1; j < n; j++) {
+      if (arr[j] < arr[mini]) {
+        mini = j;
+      }
+    }
+    int temp = arr[mini];
+    arr[mini] = arr[i];
+    arr[i] = temp;
+  }
