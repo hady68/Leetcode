751999c05cfe06ccdb929d0caa31a0666306915b
Hardikk Kamboj <64458111+khardikk@users.noreply.github.com>
2023-12-04 16:52:47 +0530

    Create Recursive_Bubble_Sort_Algorithm.cpp

---
+++ Striver A-Z Sheet/Step 2/Recursive_Bubble_Sort_Algorithm.cpp
@@ -0,0 +1,24 @@
+// Approach -> TC _> O(N)^2 SC -> O(N)
+
+
+
+void bubble_sort(int arr[], int n) {
+    // Base Case: range == 1.
+    if (n == 1) return;
+
+    int didSwap = 0;
+    for (int j = 0; j <= n - 2; j++) {
+        if (arr[j] > arr[j + 1]) {
+            int temp = arr[j + 1];
+            arr[j + 1] = arr[j];
+            arr[j] = temp;
+            didSwap = 1;
+        }
+    }
+
+    // if no swapping happens.
+    if (didSwap == 0) return;
+
+    //Range reduced after recursion:
+    bubble_sort(arr, n - 1);
+}
