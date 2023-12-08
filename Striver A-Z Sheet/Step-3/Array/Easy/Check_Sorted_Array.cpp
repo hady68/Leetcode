aab8942aeb58314ac97cca447b18557d17a960d9
Hardikk Kamboj <64458111+khardikk@users.noreply.github.com>
2023-12-08 22:05:59 +0530

    Create  Check_Sorted_Array.cpp

---
+++ Striver A-Z Sheet/Step-3/Array/Easy/Check_Sorted_Array.cpp
@@ -0,0 +1,33 @@
+// Sample Input 1 :
+// 4
+// 0 0 0 1
+// Sample Output 1 :
+// 1
+// Explanation For Sample Input 1 :
+// The given array is sorted in non-decreasing order; hence the answer will be 1. 
+// Sample Input 2 :
+// 5
+// 4 5 4 4 4
+// Sample Output 2 :
+// 0
+// Expected Time Complexity:
+// O(n), Where ‘n’ is the size of an input array ‘a’.
+// Constraints:
+// 1 ≤ 'n' ≤ 5*10^6
+// 0 ≤ 'a'[i] ≤ 10^9
+
+// Time limit: 1 sec
+
+// Brute force 
+int isSorted(int n, vector<int> a) {
+    // Iterating over the array 'A'.
+    for (int i = 0; i < n - 1; i++) {
+        if (a[i + 1] < a[i]) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+
