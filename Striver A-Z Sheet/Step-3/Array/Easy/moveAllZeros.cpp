6a2f20cb1cb1b16e8721e3876575ccdb1353bee3
Hardikk Kamboj <64458111+khardikk@users.noreply.github.com>
2024-01-02 22:31:58 +0530

    Update moveAllZeros.cpp

--- Striver A-Z Sheet/Step-3/Array/Easy/moveAllZeros.cpp
+++ Striver A-Z Sheet/Step-3/Array/Easy/moveAllZeros.cpp
@@ -47,28 +47,31 @@ vector<int> moveZeros(int n, vector<int> a) {
 // OPTIMAL APP -> Using Two Pointers 
 // TC -> O(n) SC -> O(1)
 
-vector<int> moveZeros(int n, vector<int> a) {
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
     int j = -1;
+    int n = nums.size();
     //place the pointer j:
     for (int i = 0; i < n; i++) {
-        if (a[i] == 0) {
+        if (nums[i] == 0) {
             j = i;
             break;
         }
     }
 
     //no non-zero elements:
-    if (j == -1) return a;
+    if (j == -1) return;
 
     //Move the pointers i and j
     //and swap accordingly:
     for (int i = j + 1; i < n; i++) {
-        if (a[i] != 0) {
-            swap(a[i], a[j]);
+        if (nums[i] != 0) {
+            swap(nums[i], nums[j]);
             j++;
         }
     }
-    return a;
 }
-
+    
+};
 
