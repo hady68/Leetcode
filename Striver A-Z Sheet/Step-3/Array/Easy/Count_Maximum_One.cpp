42d553b7653863599cb04e478092eed4a2610f29
Hardikk Kamboj <64458111+khardikk@users.noreply.github.com>
2024-01-04 23:34:06 +0530

    Create Count_Maximum_One.cpp

---
+++ Striver A-Z Sheet/Step-3/Array/Easy/Count_Maximum_One.cpp
@@ -0,0 +1,32 @@
+// Problem Statement: Given an array that contains only 1 and 0 return the count of maximum consecutive ones in the array.
+
+// Examples:
+
+// Example 1:
+
+// Input: prices = {1, 1, 0, 1, 1, 1}
+
+// Output: 3
+
+// Explanation: There are two consecutive 1’s and three consecutive 1’s in the array out of which maximum is 3.
+
+// Input: prices = {1, 0, 1, 1, 0, 1} 
+
+// Output: 2
+
+// Explanation: There are two consecutive 1's in the array. 
+
+ int findMaxConsecutiveOnes(vector < int > & nums) {
+      int cnt = 0;
+      int maxi = 0;
+      for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] == 1) {
+          cnt++;
+        } else {
+          cnt = 0;
+        }
+
+        maxi = max(maxi, cnt);
+      }
+      return maxi;
+    }
