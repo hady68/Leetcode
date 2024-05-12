05a814ab0304da164f410a0e70a53fc128656cf3
Hardikk Kamboj <64458111+khardikk@users.noreply.github.com>
2024-05-12 18:17:35 +0530

    Update 1512NumberGoodPairs.cpp

--- Leetcode/Cpp/Easy/1512NumberGoodPairs.cpp
+++ Leetcode/Cpp/Easy/1512NumberGoodPairs.cpp
@@ -36,3 +36,16 @@ public:
     }
 };
 
+
+// fREQUENCY 
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& A) {
+        int res = 0;
+        unordered_map<int, int> count;
+        for (int a: A) {
+            res += count[a]++;
+        }
+        return res;
+    }
+};
